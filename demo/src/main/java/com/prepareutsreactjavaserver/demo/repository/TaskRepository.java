b185e587be3e16689f647052ea372a71a876ffc5
wahyu <wabredz1234@gmail.com>
2024-07-12 17:43:38 +0700

    Hapus anotasi @Repositiry

--- demo/src/main/java/com/prepareutsreactjavaserver/demo/repository/TaskRepository.java
+++ demo/src/main/java/com/prepareutsreactjavaserver/demo/repository/TaskRepository.java
@@ -1,10 +1,8 @@
 package com.prepareutsreactjavaserver.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.prepareutsreactjavaserver.demo.models.Task;
 
-@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
