b7ce71660e3d5b437a47a7f990824a42d9eaa3f1
wahyu <wabredz1234@gmail.com>
2024-07-14 08:50:08 +0700

    add sandal repository

---
+++ demo/src/main/java/com/prepareutsreactjavaserver/demo/repository/SandalRepository.java
@@ -0,0 +1,9 @@
+package com.prepareutsreactjavaserver.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.prepareutsreactjavaserver.demo.models.Sandal;
+
+public interface SandalRepository extends JpaRepository<Sandal, Long> {
+
+}
